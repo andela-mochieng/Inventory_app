1e9c01023c1ba6f59306fc4d15f9122fc43ff564
andela-mochieng <margaret.ochieng@andela.com>
2016-12-05 14:54:29 +0300

    [Chore] create Book and Category models and register them in the admin panel

--- Inventory/search/models.py
+++ Inventory/search/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
+class Category(models.Model):
+    """Create category model"""
+    name = models.CharField(unique=True, max_length=250)
+
+    def __unicode__(self):
+        return self.name
+
+
 class Book(models.Model):
     """Creates book model"""
+    title = models.CharField(max_length=250)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+    def __unicode__(self):
+            return "{} in category {}".format(self.title, self.category)
